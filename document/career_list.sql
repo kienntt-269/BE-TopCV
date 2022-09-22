5dc5e358b3acd0511fc3949f04cef3646ebb2e4a
Nguyen Kien <nguyentienquyet525@gmail.com>
2022-09-22 22:17:35 +0700

    export data mysql

---
+++ document/career_list.sql
@@ -0,0 +1,20 @@
+/*
+-- Query: select * from career_list
+LIMIT 0, 1000
+
+-- Date: 2022-09-22 22:12
+*/
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (1,NULL,NULL,NULL,NULL,NULL,'IT - Phần mềm');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (2,NULL,NULL,NULL,NULL,NULL,'Bảo hiểm');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (3,NULL,NULL,NULL,NULL,NULL,'Bất động sản');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (4,NULL,NULL,NULL,NULL,NULL,'Chứng khoán');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (5,NULL,NULL,NULL,NULL,NULL,'Cơ khí');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (6,NULL,NULL,NULL,NULL,NULL,'Du lịch');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (7,'0','2022-09-20 21:04:23.014000',NULL,'0','2022-09-20 21:04:23.014000','Marketing / Truyền thông / Quảng cáo');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (8,'0','2022-09-20 21:04:37.647000',NULL,'0','2022-09-20 21:04:37.647000','Nhà hàng / Khách sạn');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (9,'0','2022-09-20 21:04:51.770000',NULL,'0','2022-09-20 21:04:51.770000','In ấn / Xuất bản');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (10,'0','2022-09-20 21:05:08.631000',NULL,'0','2022-09-20 21:05:08.631000','Bán lẻ - Hàng tiêu dùng');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (11,'0','2022-09-20 21:05:20.355000',NULL,'0','2022-09-20 21:05:20.355000','Sản xuất');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (12,'0','2022-09-20 21:05:26.892000',NULL,'0','2022-09-20 21:05:26.892000','Xây dựng');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (13,'0','2022-09-20 21:05:35.599000',NULL,'0','2022-09-20 21:05:35.599000','Ngân hàng');
+INSERT INTO `` (`id`,`created_by`,`created_at`,`deleted_flag`,`updated_by`,`updated_at`,`name`) VALUES (14,'0','2022-09-20 21:09:01.208000',NULL,'0','2022-09-20 21:09:01.208000','Ngân hàng');
